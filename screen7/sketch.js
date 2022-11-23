9b904d560207cdbcde3cc7da6905f82a33bb2b49
carlynorama <carlynorama@gmail.com>
2022-11-22 18:02:23 -0800

    dynamic background example

---
+++ screen7/sketch.js
@@ -0,0 +1,35 @@
+//https://www.youtube.com/watch?v=OIfEHD3KqCg
+
+var canvas;
+
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0,0);
+    canvas.style('z-index', '-1');
+    fill('#fa923f');
+    strokeWeight(5);
+    background(220);
+}
+
+function keyPressed() {
+  clear();
+}
+
+function draw() {
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+
+    var a = min(frameCount, 1200);
+    var b = min(frameCount, 400);
+    ellipse(windowWidth/2, windowHeight/3, a, b);
+    //can't do no loop if doing mouse interactions?
+    // if (frameCount > 1200) {
+    //     noLoop();
+    // }
+}
